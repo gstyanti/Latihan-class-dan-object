3bc6cf3cd5e29b175b9e4e6fa5abc6df232d78f3
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-04-24 22:49:08 +0800

    Add files via upload

---
+++ Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    public class Mahasiswa
+    {
+        //properties 
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IPK { get; set; }
+
+        //Method
+        public  void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", NIM);
+            Console.WriteLine("IPK: {0}", IPK);
+            Console.WriteLine("Anda Telah Melakukan Registrasi");
+        }
+
+        public  void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang melakukan proses KRS\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari Class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu
+            saya.NIM = "4326";
+            saya.Nama = "Yanti";
+            saya.IPK = 3.80f;
+
+            kamu.NIM = "2468";
+            kamu.Nama = "Febri";
+            kamu.IPK = 3.63f;
+
+            //memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
